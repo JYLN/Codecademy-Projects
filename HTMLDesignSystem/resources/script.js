5ac7bbfb01fd7b5a972012f301669f54c8721acc
JYLN <14062951+JYLN@users.noreply.github.com>
2023-04-08 13:40:20 -0600

    Finish project by adding JavaScript code for collapse panels

--- HTMLDesignSystem/resources/script.js
+++ HTMLDesignSystem/resources/script.js
@@ -5,3 +5,15 @@ document.querySelectorAll('pre code').forEach(el => {
 
   el.outerHTML = html.replace(new RegExp(pattern, 'g'), '\n');
 });
+
+let collapse = document.getElementsByClassName('collapse');
+
+for (let i of collapse) {
+  i.addEventListener('click', function () {
+    this.classList.toggle('active');
+    let content = this.nextElementSibling;
+    content.style.maxHeight
+      ? (content.style.maxHeight = null)
+      : (content.style.maxHeight = `${content.scrollHeight}px`);
+  });
+}
