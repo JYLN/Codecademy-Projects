f22e8633c47c6ccd810a1e884682cb475f4868fc
JYLN <14062951+JYLN@users.noreply.github.com>
2023-04-18 15:09:58 -0600

    Starting project code

---
+++ RestaurantOrders/src/index.ts
@@ -0,0 +1,12 @@
+import { Order, PriceBracket, orders } from './orders';
+import { Restaurant, restaurants } from './restaurants';
+
+/// Add your getMaxPrice() function below:
+
+/// Add your getOrders() function below:
+
+/// Add your printOrders() function below:
+
+/// Main
+// const elligibleOrders = getOrders(PriceBracket.Low, orders);
+// printOrders(restaurants, elligibleOrders);
