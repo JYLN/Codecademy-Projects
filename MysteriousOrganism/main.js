bed78649bd2b4104a85275a53a97eac1cac083ec
JYLN <14062951+JYLN@users.noreply.github.com>
2023-05-09 09:37:17 -0600

    Complete code based on project checklist

--- MysteriousOrganism/main.js
+++ MysteriousOrganism/main.js
@@ -13,10 +13,47 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+const pAequorFactory = (specimenNum, dna) => ({
+  specimenNum,
+  dna,
+  mutate() {
+    const randIdx = Math.floor(Math.random() * this.dna.length);
+    let newBase = returnRandBase();
+    while (this.dna[randIdx] === newBase) {
+      newBase = returnRandBase();
+    }
+    this.dna[randIdx] = newBase;
+    return this.dna;
+  },
+  compareDNA(otherOrgObj) {
+    const similarBases = this.dna.reduce((acc, val, idx, arr) => {
+      if (arr[idx] === otherOrgObj.dna[idx]) {
+        return acc + 1;
+      } else {
+        return acc;
+      }
+    }, 0);
 
+    const percentOfDNAShared = ((similarBases / this.dna.length) * 100).toFixed(2);
 
+    console.log(
+      `${this.specimenNum} and ${otherOrgObj.specimenNum} have ${percentOfDNAShared}% DNA in common.`
+    );
+  },
+  willLikelySurvive() {
+    const cOrG = this.dna.filter(base => base === 'C' || base === 'G');
+    return cOrG.length / this.dna.length >= 0.6;
+  },
+});
 
+const survivingOrganisms = [];
 
+for (let i = 0; survivingOrganisms.length < 30; i++) {
+  let newOrg = pAequorFactory(i, mockUpStrand());
+  if (newOrg.willLikelySurvive()) {
+    survivingOrganisms.push(newOrg);
+  }
+}
 
-
-
+console.log(survivingOrganisms);
+console.log(survivingOrganisms.length);
