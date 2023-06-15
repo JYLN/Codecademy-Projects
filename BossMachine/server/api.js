a184b9838297694f269a861a3c1e35c96d9a350e
JYLN <14062951+JYLN@users.noreply.github.com>
2023-06-15 14:11:13 -0600

    Build ideasRouter

--- BossMachine/server/api.js
+++ BossMachine/server/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const minionsRouter = require('./minions');
+const ideasRouter = require('./ideas');
 const apiRouter = express.Router();
 
 apiRouter.use('/minions', minionsRouter);
+apiRouter.use('/ideas', ideasRouter);
 
 module.exports = apiRouter;
---
+++ BossMachine/server/ideas.js
@@ -0,0 +1,46 @@
+const {
+  getAllFromDatabase,
+  addToDatabase,
+  getFromDatabaseById,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+} = require('./db');
+
+const ideasRouter = require('express').Router();
+
+ideasRouter.get('/', (req, res, next) => {
+  res.send(getAllFromDatabase('ideas'));
+});
+
+ideasRouter.post('/', (req, res, next) => {
+  const newIdea = addToDatabase('ideas', req.body);
+  res.status(201).send(newIdea);
+});
+
+ideasRouter.param('ideaId', (req, res, next, id) => {
+  const foundIdea = getFromDatabaseById('ideas', id);
+  if (foundIdea) {
+    req.idea = foundIdea;
+    return next();
+  }
+  return res.status(404).send();
+});
+
+ideasRouter.get('/:ideaId', (req, res, next) => {
+  res.send(req.idea);
+});
+
+ideasRouter.put('/:ideaId', (req, res, next) => {
+  const updatedIdea = updateInstanceInDatabase('ideas', req.body);
+  res.send(updatedIdea);
+});
+
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+  const deletedIdea = deleteFromDatabasebyId('ideas', req.idea.id);
+  if (deletedIdea) {
+    return res.status(204).send();
+  }
+  return res.status(500).send();
+});
+
+module.exports = ideasRouter;
