052e901f20acba540c7ada090518fed9ab62442a
JYLN <14062951+JYLN@users.noreply.github.com>
2023-06-15 13:17:44 -0600

    Complete code based on project checklist

--- QuoteAPI/server.js
+++ QuoteAPI/server.js
@@ -8,3 +8,35 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes/random', (req, res, next) => {
+  const randomQuote = getRandomElement(quotes);
+  res.send({ quote: randomQuote });
+});
+
+app.get('/api/quotes', (req, res, next) => {
+  const { person } = req.query;
+  if (person) {
+    const filteredQuotes = quotes.filter(
+      quote => quote.person.toLowerCase() === person.toLowerCase()
+    );
+    res.send({ quotes: filteredQuotes });
+  } else {
+    res.send({ quotes: quotes });
+  }
+});
+
+app.post('/api/quotes', (req, res, next) => {
+  const { quote, person } = req.query;
+
+  if (quote && person) {
+    const newQuote = { quote, person };
+    quotes.push(newQuote);
+    res.send({ quote: newQuote });
+  } else {
+    res.status(400).send();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running: ${PORT}`);
+});
